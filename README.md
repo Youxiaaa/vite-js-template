2b6b2cee12bea3c993d0d99b831105f70df01d99
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-08-15 22:21:47 +0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vite + Axios + pinia + 各種好用套件的開發模板 JS 版
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="w-full h-screen flex items-center justify-center">
+      <router-link to="/room">Room</router-link>
+    </div>
+  </div>
+</template>
